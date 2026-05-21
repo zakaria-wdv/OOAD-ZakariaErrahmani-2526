a99759265306085b03e3608dc5536f66a60c3e9c
zakaria-wdv <zakariaerrahmani01@gmail.com>
2026-05-21 22:54:29 +0200

    1
    ik heb dokter praktijk aangemaakt.
    ik heb een mainwindow dokter gemaakt.
    ik heb bij dokter de window gemaakt naar verlangen van de app.
    ik heb een dokter gekopieerd en ervan patient gemaakt.

---
+++ SlnProject/DoktersPraktijk/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DoktersPraktijk
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
